e02454b49a43fde0607a6724968907a7c7598ba4
Casey Nalley <cnalley@windhoverlabs.com>
2018-11-10 12:27:21 -0600

    Implements #711 in progress adding BMP280 application (PIC).

---
+++ fsw/mission_inc/bmp280_perfids.h
@@ -0,0 +1,21 @@
+#ifndef BMP280_PERFIDS_H
+#define BMP280_PERFIDS_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* #include "perf_ids.h" */
+#define BMP280_MAIN_TASK_PERF_ID    115
+#define BMP280_RECEIVE_PERF_ID      116
+#define BMP280_SEND_PERF_ID         117
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* BMP280_PERFIDS_H */
+
+/************************/
+/*  End of File Comment */
+/************************/
---
+++ fsw/platform_inc/bmp280_msgids.h
@@ -0,0 +1,25 @@
+#ifndef BMP280_MSGIDS_H
+#define BMP280_MSGIDS_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* #include "msg_ids.h" */
+
+/* BMP280                                               */
+#define BMP280_SEND_HK_MID                          0x0cb5
+#define BMP280_HK_TLM_MID                           0x0cb6
+#define BMP280_DIAG_TLM_MID                         0x0cb7
+#define BMP280_CMD_MID                              0x0cb8
+#define BMP280_MEASURE_MID                          0x0cb9
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* BMP280_MSGIDS_H */
+
+/************************/
+/*  End of File Comment */
+/************************/
